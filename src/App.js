f90a9ce07cabf9f16d969f37b0a5c5a741707d20
RaidenManiac <pratyaksh02@gmail.com>
2023-04-01 12:30:59 -0400

    adminpage1.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom'
 import "./App.css";
 import LoginRegister from "./pages/login_register/LoginRegister";
 import Home from "./pages/Home/Home";
 import MainPage from "./pages/MainPage/src/MainPage";
+import AdminPage from "./pages/Admin page/src/Admin"
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/login" element={ <LoginRegister/> } />
         <Route exact path="/" element={<Home/>} />
         <Route exact path="/start" element={<MainPage/>} />
+        <Route exact path="/Home" element={<AdminPage/>} />
       </Routes>
     </Router>
   );
