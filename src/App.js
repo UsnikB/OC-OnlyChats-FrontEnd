eca34a17f9d89ff997adc4d3dc6fb1280f1a349c
RaidenManiac <pratyaksh02@gmail.com>
2023-04-01 13:03:00 -0400

    FixingAdminpageissue

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom'
 import "./App.css";
 import LoginRegister from "./pages/login_register/LoginRegister";
 import Home from "./pages/Home/Home";
-<<<<<<< HEAD
-import MainPage from "./pages/MainPage/src/MainPage";
-import AdminPage from "./pages/Admin page/src/Admin"
-=======
 import MainPage from "./pages/MainPage/MainPage";
->>>>>>> 069b5290c61007ff143d4fcd0f6581a405f8655c
+import AdminPage from "./pages/Adminpage/src/Admin";
+
 
 function App() {
 
@@ -19,7 +16,7 @@ function App() {
         <Route exact path="/login" element={ <LoginRegister/> } />
         <Route exact path="/" element={<Home/>} />
         <Route exact path="/start" element={<MainPage/>} />
-        <Route exact path="/Home" element={<AdminPage/>} />
+        <Route exact path="/admin" element={<AdminPage/>} />
       </Routes>
     </Router>
   );
